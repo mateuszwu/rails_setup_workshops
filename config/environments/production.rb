df970a3490e3c732a44175a8249ce07421ec6471
Mateusz Wieczorek <mateusz.wu@gmail.com>
2022-10-12 11:59:56 +0200

    add render.com setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,6 +7,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.hosts << 'railssetupworkshops-production.up.railway.app'
+  config.hosts << 'rails-setup-workshops.onrender.com'
   config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
@@ -25,7 +26,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
